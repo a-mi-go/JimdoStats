d87ac32190052c8241ada779cf0d87563125f743
muxagen <mihahh@gmail.com>
2015-06-22 04:29:35 +0200

    delete Android Application Test, add Junit Test

---
+++ app/src/test/java/simpleTest.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2015 Mikhail Goldenzweig
+ * MIT Licence
+ */
+
+import org.junit.Test;
+import android.test.suitebuilder.annotation.SmallTest;
+import static org.junit.Assert.*;
+
+@SmallTest
+public class simpleTest {
+    @Test
+    public void testTwoPlusTwo() {
+
+        long actual = 2 + 2;
+        long expected = 4;
+        assertEquals("2 + 2 filed", expected, actual);
+    }
+
+}
