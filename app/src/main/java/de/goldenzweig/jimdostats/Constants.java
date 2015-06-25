3ffea5103c225092e6940b467d48e830f31835fc
muxagen <mihahh@gmail.com>
2015-06-26 01:45:26 +0200

    comments

--- app/src/main/java/de/goldenzweig/jimdostats/Constants.java
+++ app/src/main/java/de/goldenzweig/jimdostats/Constants.java
@@ -4,8 +4,9 @@
  */
 package de.goldenzweig.jimdostats;
 
-public class Constants {
-    
+public final class Constants {
+    // Number of days in a week
     public static final int WEEK_DAYS = 7;
+    // Approximation of number of days in a month
     public static final int MONTH_DAYS = 30;
 }
